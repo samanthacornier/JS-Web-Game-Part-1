5224a628cd2a1d92d98bf0ff89f0b248ff75eb95
samanthacornier <samantha112069@gmail.com>
2022-10-22 13:50:14 -0400

    first js assignment

--- index.js
+++ index.js
@@ -1,6 +1,122 @@
 let greenCharacter = document.createElement('img')
+function newimage (greenCharacter){greenCharacter.src = 'assets/green-character.gif'
+greenCharacter.style.position = 'fixed'
+greenCharacter.style.left = '100px'
+greenCharacter.style.bottom = '100px'
+document.body.append(greenCharacter)}
+newimage(greenCharacter)
+
 greenCharacter.src = 'assets/green-character.gif'
 greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
 document.body.append(greenCharacter)
+
+
+let pineTree = document.createElement('img')
+function newimage (){pineTree}
+newimage(pineTree) 
+
+pineTree.src = 'assets/pine-tree.png'
+pineTree.style.position = 'fixed'
+pineTree.style.left = '450px'
+pineTree.style.bottom = '200px'
+document.body.append(pineTree)
+
+
+
+let Tree = document.createElement('img')
+function newimage (){Tree}
+newimage(Tree)
+
+Tree.src = 'assets/tree.png'
+Tree.style.position = 'fixed'
+Tree.style.left = '200px'
+Tree.style.bottom = '300px'
+document.body.append(Tree)
+
+
+
+let pillar = document.createElement('img')
+function newimage() {pillar}
+newimage(pillar)
+
+pillar.src = 'assets/pillar.png'
+pillar.style.position = 'fixed'
+pillar.style.left = '350px'
+pillar.style.bottom = '100px'
+document.body.append(pillar)
+
+
+
+
+let crate = document.createElement('img')
+function newimage(params) {crate}
+newimage(crate)
+
+crate.src = 'assets/crate.png'
+crate.style.position = 'fixed'
+crate.style.left = '150px'
+crate.style.bottom = '200px'
+document.body.append(crate)
+
+
+
+
+let well = document.createElement('img')
+function newimage(params) {well}
+newimage(well)
+
+well.src = 'assets/well.png'
+well.style.position = 'fixed'
+well.style.left = '500px'
+well.style.bottom = '425px'
+document.body.append(well)
+
+
+
+
+let sword = document.createElement('img')
+function newitem() {sword}
+newitem (sword)
+
+sword.src = 'assets/sword.png'
+sword.position = 'fixed'
+sword.left = '500px'
+sword.bottom = '405px'
+document.body.append(sword)
+
+sword.addEventListener('dblclick', function(){
+    sword.remove()
+})
+
+let shield = document.createElement('img')
+function newitem(){shield}
+newitem(shield)
+
+shield.src = 'assets/shield.png'
+shield.position = 'fixed'
+shield.left = '165px'
+shield.bottom = '185px'
+document.body.append(shield)
+
+
+
+let staff = document.createElement('img')
+function newitem(){staff}
+newitem(staff)
+
+staff.src = 'assets/staff.png'
+staff.position = 'fixed'
+staff.left = '165px'
+staff.bottom = '185px'
+document.body.append(staff)
+
+
+
+
+
+
+
+
+
