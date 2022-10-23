b6dd8308bf5939bebc47685b1190b701d9fc1129
samanthacornier <samantha112069@gmail.com>
2022-10-23 00:53:28 -0400

    made changes to JS assignment

--- index.js
+++ index.js
@@ -1,116 +1,56 @@
 let greenCharacter = document.createElement('img')
-function newimage (greenCharacter){greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)}
-newimage(greenCharacter)
-
 greenCharacter.src = 'assets/green-character.gif'
 greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
 document.body.append(greenCharacter)
 
-
 let pineTree = document.createElement('img')
-function newimage (){pineTree}
-newimage(pineTree) 
-
 pineTree.src = 'assets/pine-tree.png'
 pineTree.style.position = 'fixed'
 pineTree.style.left = '450px'
 pineTree.style.bottom = '200px'
 document.body.append(pineTree)
 
-
-
-let Tree = document.createElement('img')
-function newimage (){Tree}
-newimage(Tree)
-
-Tree.src = 'assets/tree.png'
-Tree.style.position = 'fixed'
-Tree.style.left = '200px'
-Tree.style.bottom = '300px'
-document.body.append(Tree)
+function newImage(url, left, bottom){
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object}
 
 
 
-let pillar = document.createElement('img')
-function newimage() {pillar}
-newimage(pillar)
-
-pillar.src = 'assets/pillar.png'
-pillar.style.position = 'fixed'
-pillar.style.left = '350px'
-pillar.style.bottom = '100px'
-document.body.append(pillar)
-
-
-
-
-let crate = document.createElement('img')
-function newimage(params) {crate}
-newimage(crate)
-
-crate.src = 'assets/crate.png'
-crate.style.position = 'fixed'
-crate.style.left = '150px'
-crate.style.bottom = '200px'
-document.body.append(crate)
-
-
-
-
-let well = document.createElement('img')
-function newimage(params) {well}
-newimage(well)
-
-well.src = 'assets/well.png'
-well.style.position = 'fixed'
-well.style.left = '500px'
-well.style.bottom = '425px'
-document.body.append(well)
-
-
-
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/tree.png', 200, 300)
 
 let sword = document.createElement('img')
-function newitem() {sword}
-newitem (sword)
-
 sword.src = 'assets/sword.png'
 sword.position = 'fixed'
 sword.left = '500px'
 sword.bottom = '405px'
 document.body.append(sword)
 
-sword.addEventListener('dblclick', function(){
+sword.addEventListener('click', function(){
     sword.remove()
 })
 
-let shield = document.createElement('img')
-function newitem(){shield}
-newitem(shield)
-
-shield.src = 'assets/shield.png'
-shield.position = 'fixed'
-shield.left = '165px'
-shield.bottom = '185px'
-document.body.append(shield)
 
 
 
-let staff = document.createElement('img')
-function newitem(){staff}
-newitem(staff)
 
-staff.src = 'assets/staff.png'
-staff.position = 'fixed'
-staff.left = '165px'
-staff.bottom = '185px'
-document.body.append(staff)
+
+
+
+
+
+
 
 
 
